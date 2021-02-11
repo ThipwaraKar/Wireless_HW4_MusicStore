0dade662301eeed13696114b4a1e47758dd209f9
ThipwaraKar <55127472+ThipwaraKar@users.noreply.github.com>
2021-02-11 21:02:54 +0700

    AddListview

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,577 @@
 import 'package:flutter/material.dart';
-void main (){
-  runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
 
-      return MaterialApp(
-        home: MyHomepage(),
-      );
-    }
-  }
-  class MyHomepage extends StatefulWidget {
-  @override
-  _MyHomepageState createState() => _MyHomepageState();
-  }
-class _MyHomepageState extends State<MyHomepage> {
-  int number = 0; // Create state variable
-  @override
+
+void main() => runApp(MyApp());
+
+ class MyApp extends StatelessWidget {
+  int number = 0;
+   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Count Number',),
-      ),
-      body: Center(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center ,
-      children: [Text('Press Button to Increase Number', style: TextStyle( fontSize: 20), ),
-        Text(number.toString(),style: TextStyle(fontSize: 40),)],
-      ),
-      ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: addNumber,
-          child: Icon(Icons.add)),
-    );
+   return MaterialApp(
+  debugShowCheckedModeBanner: false,
+   title: 'TheMint Music store',
+   theme: ThemeData(
+   primarySwatch: Colors.amber,
+   ),
+       home: Scaffold(
+   appBar: AppBar(title: Text('TheMint Music store',),
+
+   ),
+
+   body: BodyLayout(),
+
+
+   ),
+   );
+   }
+   }
+
+ class BodyLayout extends StatelessWidget {
+  int number = 0;
+   @override
+   Widget build(BuildContext context) {
+
+   return _myListView(context);
   }
-  void addNumber() {
-    setState(() {
-      number++;
-    });
-  }
+   }
+
+
+
+
+
+
+ // replace this function with the code in the examples
+ Widget _myListView(BuildContext context) {
+  int number = 0;
+   return ListView(
+       children: <Widget>[
+        Container(
+         child:
+         Text(number.toString(),style: TextStyle(fontSize: 40),),
+
+
+        ),
+    ListTile(
+    leading: CircleAvatar(
+    backgroundImage: AssetImage('images/music1.jpg'),
+    ),
+    title: Text('Tom Yum'),
+    subtitle: Text('Price: \$4'),
+    trailing: Icon(Icons.keyboard_arrow_right),
+    onTap: () {
+    print('Tom Yum');
+
+    },
+    ),
+    ListTile(
+    leading: CircleAvatar(
+    backgroundImage: AssetImage('images/music2.jpg'),
+    ),
+    title: Text('Bread'),
+    subtitle: Text('Price: \$5'),
+    trailing: Icon(Icons.my_library_music_sharp),
+    onTap: () {
+    print('Bread');
+    },
+    ),
+    ListTile(
+    leading: CircleAvatar(
+    backgroundImage: AssetImage('images/music2.jpg'),
+    ),
+    title: Text('Burger'),
+    subtitle: Text('Price: \$6'),
+    trailing: Icon(Icons.keyboard_arrow_right),
+    onTap: () {
+    print('Burger');
+   },
+    ),
+   ListTile(
+   leading: CircleAvatar(
+    backgroundImage: AssetImage('images/music2.jpg'),
+    ),
+   title: Text('Food Frence Toast'),
+    subtitle: Text('Price: \$10'),
+   trailing: Icon(Icons.keyboard_arrow_right),
+    onTap: () {
+    print('Frence Toast');
+    },
+    ),
+    ListTile(
+   leading: CircleAvatar(
+    backgroundImage: AssetImage('images/music2.jpg'),
+    ),
+    title: Text('Kollam'),
+    subtitle: Text('Price: \$5'),
+    trailing: Icon(Icons.keyboard_arrow_right),
+    onTap: () {
+    print('Kollam');
+   },
+   ),
+        ListTile(
+         leading: CircleAvatar(
+          backgroundImage: AssetImage('images/music2.jpg'),
+         ),
+         title: Text('Kollam'),
+         subtitle: Text('Price: \$5'),
+         trailing: Icon(Icons.keyboard_arrow_right),
+         onTap: () {
+          print('Kollam');
+         },
+        ),
+        ListTile(
+         leading: CircleAvatar(
+          backgroundImage: AssetImage('images/music2.jpg'),
+         ),
+         title: Text('Kollam'),
+         subtitle: Text('Price: \$5'),
+         trailing: Icon(Icons.keyboard_arrow_right),
+         onTap: () {
+          print('Kollam');
+         },
+        ),
+        ListTile(
+         leading: CircleAvatar(
+          backgroundImage: AssetImage('images/music2.jpg'),
+         ),
+         title: Text('Kollam'),
+         subtitle: Text('Price: \$5'),
+         trailing: Icon(Icons.keyboard_arrow_right),
+         onTap: () {
+          print('Kollam');
+         },
+        ),
+        ListTile(
+         leading: CircleAvatar(
+          backgroundImage: AssetImage('images/music2.jpg'),
+         ),
+         title: Text('Kollam'),
+         subtitle: Text('Price: \$5'),
+         trailing: Icon(Icons.keyboard_arrow_right),
+         onTap: () {
+          print('Kollam');
+         },
+        ),
+        ListTile(
+         leading: CircleAvatar(
+          backgroundImage: AssetImage('images/music2.jpg'),
+         ),
+         title: Text('Kollam'),
+         subtitle: Text('Price: \$5'),
+         trailing: Icon(Icons.keyboard_arrow_right),
+         onTap: () {
+          print('Kollam');
+         },
+        ),
+
+
+    ],
+   );
+
+
+
+ }
+
+
+/*import 'package:flutter/material.dart';
+
+void main() {
+ runApp(Home());
 }
+
+class Home extends StatefulWidget {
+ @override
+ _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ Future<List<double>> getData() async {
+  return items;
+ }
+
+ Future<double> removeIt(int index) async {
+  return items.removeAt(index);
+ }
+
+ List<double> items = [100.0, 200.0, 300.0, 400.0];
+
+ @override
+ Widget build(BuildContext context) {
+  return MaterialApp(
+      home: Scaffold(
+       body: FutureBuilder<List<double>>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot<List<double>> snapshot) {
+         if (snapshot.hasData)
+          return Column(
+           children: <Widget>[
+            ListView.builder(
+             shrinkWrap: true,
+             itemCount: snapshot.data.length,
+             itemBuilder: (BuildContext context, int index) => ListTile(
+              title: Text('Items : ${snapshot.data[index]}'),
+              trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () async {
+                   await removeIt(index);
+                   setState(() {});
+                  }),
+             ),
+            ),
+            Container(
+             child:
+             Text('Sum is: ${snapshot.data.fold(0, (a, b) => a + b)}'),
+            )
+           ],
+          );
+
+         return Center(child: CircularProgressIndicator());
+        },
+       ),
+      ));
+ }
+}*/
+
+/*
+class cartConstant {
+ static int packageCount;
+ static List<int> list;
+}
+
+class OrderPage extends StatefulWidget {
+ @override
+ _OrderPageState createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+ int data = 3;
+
+ void refresh() {
+  setState(() {});
+ }
+
+ @override
+ Widget build(BuildContext context) {
+  return Scaffold(
+   backgroundColor: Colors.white,
+   appBar: AppBar(
+    automaticallyImplyLeading: true,
+    iconTheme: IconThemeData(
+     color: Colors.black54, //change your color here
+    ),
+    backgroundColor: Colors.white,
+    elevation: 1,
+    title: Text(
+     "Your order Summery",
+     style: TextStyle(color: Colors.black54),
+    ),
+    centerTitle: true,
+   ),
+   body: Container(
+    child: FutureBuilder(
+     builder: (context, snapshot) {
+//            var datas = snapshot.data;
+      return ListView.builder(
+       physics: ClampingScrollPhysics(),
+       shrinkWrap: true,
+       itemCount: data,
+       itemBuilder: (BuildContext context, int index) {
+//                Cart cart = datas[index];
+        return CartListView(refresh);
+       },
+       padding: EdgeInsets.symmetric(horizontal: 10.0),
+       scrollDirection: Axis.vertical,
+      );
+     },
+    ),
+   ),
+   bottomNavigationBar: _buildTotalContainer(),
+  );
+ }
+
+ Widget _buildTotalContainer() {
+  return Container(
+   height: 220.0,
+   padding: EdgeInsets.only(
+    left: 10.0,
+    right: 10.0,
+   ),
+   child: Column(
+    children: <Widget>[
+     SizedBox(
+      height: 10.0,
+     ),
+     Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Row(
+       mainAxisSize: MainAxisSize.max,
+       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+       children: <Widget>[
+        Text(
+         "Subtotal",
+         style: TextStyle(
+             color: Color(0xFF9BA7C6),
+             fontSize: 16.0,
+             fontWeight: FontWeight.bold),
+        ),
+        Text(
+         cartConstant.packageCount.toString(),
+         style: TextStyle(
+             color: Color(0xFF6C6D6D),
+             fontSize: 16.0,
+             fontWeight: FontWeight.bold),
+        ),
+       ],
+      ),
+     ),
+     SizedBox(
+      height: 15,
+     ),
+     Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+       Text(
+        "Discount",
+        style: TextStyle(
+            color: Color(0xFF9BA7C6),
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold),
+       ),
+       Text(
+        "0.0",
+        style: TextStyle(
+            color: Color(0xFF6C6D6D),
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold),
+       ),
+      ],
+     ),
+     SizedBox(
+      height: 10.0,
+     ),
+     Divider(
+      height: 2.0,
+     ),
+     SizedBox(
+      height: 20.0,
+     ),
+     Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+       Text(
+        "Cart Total",
+        style: TextStyle(
+            color: Color(0xFF9BA7C6),
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold),
+       ),
+       Text(
+        "8000",
+        style: TextStyle(
+            color: Color(0xFF6C6D6D),
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold),
+       ),
+      ],
+     ),
+     SizedBox(
+      height: 20.0,
+     ),
+     GestureDetector(
+      onTap: () {
+//              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => SignInPage()));
+      },
+      child: Container(
+       height: 50.0,
+       decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(35.0),
+       ),
+       child: Center(
+        child: Text(
+         "Proceed To Checkout",
+         style: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+         ),
+        ),
+       ),
+      ),
+     ),
+     SizedBox(
+      height: 20.0,
+     ),
+    ],
+   ),
+  );
+ }
+}
+
+class CartListView extends StatefulWidget {
+ VoidCallback callback;
+
+ CartListView(this.callback);
+ @override
+ _CartListViewState createState() => _CartListViewState();
+}
+
+class _CartListViewState extends State<CartListView> {
+ int _counter = 1;
+ int getPrice(int i, int priceC) {
+  cartConstant.packageCount = i * priceC;
+  return cartConstant.packageCount;
+ }
+
+ @override
+ Widget build(BuildContext context) {
+  return Card(
+   child: Padding(
+    padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+    child: Row(
+     mainAxisSize: MainAxisSize.max,
+     crossAxisAlignment: CrossAxisAlignment.start,
+     children: <Widget>[
+      Container(
+       decoration: BoxDecoration(
+        border: Border.all(color: Color(0xFFD3D3D3), width: 2.0),
+        borderRadius: BorderRadius.circular(10.0),
+       ),
+       child: Padding(
+        padding: EdgeInsets.symmetric(
+         horizontal: 10.0,
+        ),
+        child: Column(
+         crossAxisAlignment: CrossAxisAlignment.center,
+         children: <Widget>[
+          InkWell(
+              onTap: () {
+               setState(() {
+                _counter++;
+                if (_counter > 20) {
+                 _counter = 20;
+                }
+               });
+
+               widget.callback();
+              },
+              child: Icon(Icons.add, color: Color(0xFFD3D3D3))),
+          Text(
+           "$_counter",
+           style: TextStyle(fontSize: 18.0, color: Colors.grey),
+          ),
+          InkWell(
+              onTap: () {
+               setState(() {
+                _counter--;
+                if (_counter < 2) {
+                 _counter = 1;
+                }
+               });
+
+               widget.callback();
+              },
+              child: Icon(Icons.remove, color: Color(0xFFD3D3D3))),
+         ],
+        ),
+       ),
+      ),
+      SizedBox(
+       width: 20.0,
+      ),
+      Container(
+       height: 70.0,
+       width: 70.0,
+       decoration: BoxDecoration(
+           image: DecorationImage(
+               image: AssetImage("images/music2.jpg"),
+               fit: BoxFit.cover),
+           borderRadius: BorderRadius.circular(35.0),
+           boxShadow: [
+            BoxShadow(
+                color: Colors.black54,
+                blurRadius: 5.0,
+                offset: Offset(0.0, 2.0))
+           ]),
+      ),
+      SizedBox(
+       width: 20.0,
+      ),
+      Column(
+       mainAxisAlignment: MainAxisAlignment.start,
+       crossAxisAlignment: CrossAxisAlignment.start,
+       children: <Widget>[
+        Text(
+         "Employee Package",
+         style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 5.0),
+        SizedBox(height: 5.0),
+        Container(
+         height: 25.0,
+         width: 120.0,
+         child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+           Row(
+            children: <Widget>[
+             Text("Price",
+                 style: TextStyle(
+                     color: Color(0xFFD3D3D3),
+                     fontWeight: FontWeight.bold)),
+             SizedBox(
+              width: 5.0,
+             ),
+             InkWell(
+              onTap: () {},
+              child: Text(
+               getPrice(_counter, 2000).toString(),
+               style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+               ),
+              ),
+             ),
+             SizedBox(
+              width: 10.0,
+             ),
+            ],
+           ),
+          ],
+         ),
+        ),
+       ],
+      ),
+      Spacer(),
+      GestureDetector(
+       onTap: () {},
+       child: Icon(
+        Icons.cancel,
+        color: Colors.grey,
+       ),
+      ),
+     ],
+    ),
+   ),
+  );
+ }
+}
+
+void main() {
+ runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+  return MaterialApp(
+   title: 'Flutter Demo',
+   theme: ThemeData(
+    primarySwatch: Colors.blue,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+   ),
+   home: OrderPage(),
+  );
+ }
+}*/
